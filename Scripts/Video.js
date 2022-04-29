efe269d59e921ae67b23a6a58739860e2aafd197
Lokefs3000 <95932279+Lokefs3000@users.noreply.github.com>
2022-04-29 14:17:03 +0200

    Update Video.js

--- Scripts/Video.js
+++ Scripts/Video.js
@@ -1,6 +1,6 @@
 //get website link
 var website = window.location.href;
-var trimmed = website.substring(website.indexOf("/", 10) + 12);
+var trimmed = website.substring(website.indexOf("/", 10) + 19);
 
 var hasloaded = false;
 var isPause = false;
